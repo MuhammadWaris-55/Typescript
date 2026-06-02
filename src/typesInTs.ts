031f5acd169f0f20ba1f2896d9dd371fa620ea48
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-06-02 22:58:19 +0500

    Learning Typescript

--- src/typesInTs.ts
+++ src/typesInTs.ts
@@ -6,3 +6,8 @@ let horsePower = 650;
 
 let noOfSeats = Math.random() > 1 ? 2 : 4
 
+//Type Annotations
+
+let color:string = "Matte Black"
+color = "Mettalic Green"
+// color = 666 //This will give an error
