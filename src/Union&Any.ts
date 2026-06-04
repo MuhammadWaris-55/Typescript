39abc8021b6c7910b10a5ef5967f10604bf209c4
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-06-04 12:01:43 +0500

    Union and Any

--- src/Union&Any.ts
+++ src/Union&Any.ts
@@ -1,2 +1,5 @@
 //Union
 let CarModel: number | string = 2022 //its value can either be a number or a string
+
+let apiRequestStatus: "pending" | "success" | "error" = "pending"
+apiRequestStatus = "success"
