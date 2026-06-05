3a846628be0638e8a44ea00530d2a2394a092899
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-06-06 00:09:05 +0500

    Type Narrow

--- src/typeNarrow.ts
+++ src/typeNarrow.ts
@@ -6,10 +6,25 @@ function carModel(model: string | number) {
   return `Model Year ${model}`; //And if we put "." here we get all methods of number
 }
 
+//truthful checking
+function carDelivered(msg?: string) {
+  //putting ? menas we dont know we will get the msg or not
+  if (msg) {
+    return `Car is ${msg}`;
+  }
+  return `Car is not Delivered`; // we dont have to use msg variable here becase we r sure we get this output when our msg has not came
+}
 
-function carDelivered(msg?: string){ //putting ? menas we dont know we will get the msg or not
-    if(msg){
-        return `Car is ${msg}`
-    }
-    return `Car is not Delivered` // we dont have to use msg variable here becase we r sure we get this output when our msg has not came 
+//EXhaustive Checking
+function carName(name: "BMW" | "Mercedes" | "Aston Martin" | number) {
+  if (name === "BMW") {
+    return `BMW M5 CS`;
+  }
+  if (name === "Mercedes") {
+    return `Mercedes-AMG GT`;
+  }
+  if (name === "Aston Martin") {
+    return `Aston Martin Valkyrie`;
+  }
+  return `Model Number of car is ${name}`;
 }
