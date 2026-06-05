c7baf1e0551298748d6793b9fd52841e5302c841
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-06-05 23:57:30 +0500

    Type Narrow

--- src/typeNarrow.ts
+++ src/typeNarrow.ts
@@ -5,3 +5,11 @@ function carModel(model: string | number) {
   }
   return `Model Year ${model}`; //And if we put "." here we get all methods of number
 }
+
+
+function carDelivered(msg?: string){ //putting ? menas we dont know we will get the msg or not
+    if(msg){
+        return `Car is ${msg}`
+    }
+    return `Car is not Delivered` // we dont have to use msg variable here becase we r sure we get this output when our msg has not came 
+}
