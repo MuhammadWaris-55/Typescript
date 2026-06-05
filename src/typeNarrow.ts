bca355d9d9d844b38ad503077db964b2324e71ad
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-06-05 23:52:00 +0500

    Type Narrow

--- src/typeNarrow.ts
+++ src/typeNarrow.ts
@@ -1,3 +1,4 @@
+//Type Narrowing
 function carModel(model: string | number){
     if(typeof model === 'string'){
         return `Model of this BMW is ${model}` //the advantage of doing this is when we write "." in front of model we get all the methods of Strings
