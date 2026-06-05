dbc52301e7365202785b08e269c641a879490e4c
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-06-05 23:52:20 +0500

    Type Narrow

--- src/typeNarrow.ts
+++ src/typeNarrow.ts
@@ -1,7 +1,7 @@
 //Type Narrowing
-function carModel(model: string | number){
-    if(typeof model === 'string'){
-        return `Model of this BMW is ${model}` //the advantage of doing this is when we write "." in front of model we get all the methods of Strings
-    }
-    return `Model Year ${model}` //And if we put "." here we get all methods of number
+function carModel(model: string | number) {
+  if (typeof model === "string") {
+    return `Model of this BMW is ${model}`; //the advantage of doing this is when we write "." in front of model we get all the methods of Strings
+  }
+  return `Model Year ${model}`; //And if we put "." here we get all methods of number
 }
