3ab8ad3c822a8975c5e1abd61fb81785ce41ca2f
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-06-05 23:51:16 +0500

    Type Narrow

--- src/typeNarrow.ts
+++ src/typeNarrow.ts
@@ -1,6 +1,6 @@
 function carModel(model: string | number){
     if(typeof model === 'string'){
-        return `Model of this BMW is ${model}`
+        return `Model of this BMW is ${model}` //the advantage of doing this is when we write "." in front of model we get all the methods of Strings
     }
-    return `Model Year ${model}`
+    return `Model Year ${model}` //And if we put "." here we get all methods of number
 }
