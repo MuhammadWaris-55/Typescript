b20ccc76f4cc7e1d46c8b4d30a31a10c845affc3
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-06-08 16:00:50 +0500

    Type Assertion , Unknown , Never

--- src/MoreTypes.ts
+++ src/MoreTypes.ts
@@ -1,16 +1,39 @@
 //Forcefull Type Assertion
 let response: any = "32";
 
-let numericLength:number = (response as string).length
+let numericLength: number = (response as string).length;
 
 type Car = {
-    name: string
+  name: string;
+};
+
+let CarString = '{"name":"BMW"}';
+let CarObject = JSON.parse(CarString) as Car;
+
+console.log(CarObject);
+
+const InputElement = document.getElementById("username") as HTMLInputElement;
+
+//Use of Unknown
+
+//This will not give an error because it uses any
+// let value:any;
+// value = "Car"
+// value = [1,2,3]
+// value = 3.2
+// value.toUpperCase()
+
+//Now this will give an error
+// let NewValue:unknown;
+// NewValue = "Car"
+// NewValue = [1,2,3]
+// NewValue = 3.2
+// NewValue.toUpperCase()
+
+let NewValue: unknown;
+NewValue = "Car";
+NewValue = [1, 2, 3];
+NewValue = 3.2;
+if (typeof NewValue === "string") {
+  NewValue.toUpperCase();
 }
-
-let CarString = '{"name":"BMW"}'
-let CarObject = JSON.parse(CarString) as Car 
-
-console.log(CarObject)
-
-
-const InputElement = document.getElementById("username") as HTMLInputElement
