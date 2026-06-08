6c1b3d0065c147d6519d3914f0c2c8a0e43335eb
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-06-08 15:50:06 +0500

    Type Assertion , Unknown , Never

--- src/MoreTypes.ts
+++ src/MoreTypes.ts
@@ -2,3 +2,12 @@
 let response: any = "32";
 
 let numericLength:number = (response as string).length
+
+type Car = {
+    name: string
+}
+
+let CarString = '{"name":"BMW"}'
+let CarObject = JSON.parse(CarString) as Car 
+
+console.log(CarObject)
