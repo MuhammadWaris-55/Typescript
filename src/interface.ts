ddbecbe75397fadcbcc40b09e88945d6b65e1105
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-06-11 16:35:39 +0500

    Interface in Typescript

--- src/interface.ts
+++ src/interface.ts
@@ -1,7 +1,23 @@
-function Car(features: {type: string ; horsepower: number ; sedan: boolean}) {
+// function Car(features: {type: string ; horsepower: number ; sedan: boolean}) {
+//     console.log(features);
+// }
+
+// function DeliverCar(features: {type: string ; horsepower: number ; sedan: boolean}) {
+//     console.log(features);
+// }
+//Here the Signature Of data is same so we dont have to write it again and again
+
+type CarFeatures = {
+    type: string ;
+    horsepower: number ;
+    sedan: boolean
+}
+//now we can write same logic as
+
+function Car(features: CarFeatures) {
     console.log(features);
 }
 
-function DeliverCar(features: {type: string ; horsepower: number ; sedan: boolean}) {
+function DeliverCar(features: CarFeatures) {
     console.log(features);
 }
