8f6ec3a452b8729e4411fcf33ab096ee65e55272
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-06-11 16:32:36 +0500

    Interface in Typescript

---
+++ src/interface.ts
@@ -0,0 +1,7 @@
+function Car(features: {type: string ; horsepower: number ; sedan: boolean}) {
+    console.log(features);
+}
+
+function DeliverCar(features: {type: string ; horsepower: number ; sedan: boolean}) {
+    console.log(features);
+}
