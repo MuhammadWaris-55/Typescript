27bf0b305d2bf7ba6140eea689e68739cd0b94e0
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-06-16 23:44:28 +0500

    learn Functions

--- src/Functions.ts
+++ src/Functions.ts
@@ -2,4 +2,4 @@ function makeCar(type: string, fueltank: number){
     console.log(`Making a ${type} Car which has a ${fueltank} litre of fuelTank`);
 }
 
-makeCar("Sedan", 50)
+makeCar("Sedan", 50) //if we don't give the no of arguments it needed here then it will give an error , that's the main purpose of using typescript
