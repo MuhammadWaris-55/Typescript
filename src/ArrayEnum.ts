ad24c2cca9eb43436f573d6b4bab966d7bf25ac9
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-06-18 12:06:51 +0500

    learn array & enum

--- src/ArrayEnum.ts
+++ src/ArrayEnum.ts
@@ -2,3 +2,6 @@
 const CarColors: string[] = ["Matt Black" , "Royal Blue"]
 
 const CarPrice: number[] = [340, 400]
+
+//we can also give custom type here
+const highRating: Array<boolean> = [true, false]
