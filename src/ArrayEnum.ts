fcacd515773f3e89b4886e7465741cb564bcb70a
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-06-18 12:10:18 +0500

    learn array & enum

--- src/ArrayEnum.ts
+++ src/ArrayEnum.ts
@@ -5,3 +5,15 @@ const CarPrice: number[] = [340, 400]
 
 //we can also give custom type here
 const highRating: Array<boolean> = [true, false]
+
+
+type Car = {
+    name: string;
+    price: number;
+    color: string;
+}
+
+const car: Car[] = [
+    {name: "BMW" , price: 340 , color: "Black"},
+    {name: "Mercedes" , price: 250 , color: "White"},
+]
